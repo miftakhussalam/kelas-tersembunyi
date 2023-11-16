83bbc608f080c108823769c9652cb8677fce96c6
Miftakhus Salam <miftakhussalam@ChawpyTuta.localdomain>
2023-11-16 18:19:24 +0700

    <feat>(common): styling react

---
+++ hello-app/src/App.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div
+      className="App"
+      // style={{
+      //   display: "flex",
+      //   justifyContent: "center",
+      //   alignItems: "center",
+      //   backgroundColor: '#282c34',
+      //   padding: '10px',
+      //   fontSize: '22px',
+      //   fontWeight: '900',
+      //   color: 'antiquewhite'
+      // }}
+    >
+      hello world!
+    </div>
+  );
+}
+
+export default App;
